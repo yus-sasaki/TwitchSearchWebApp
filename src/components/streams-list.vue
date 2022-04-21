47a0038d865a082d45e97cda1a062939231a5ef3
yus-sasaki <71699816+yus-sasaki@users.noreply.github.com>
2022-04-21 16:03:01 +0900

    F チャンネル検索のテスト

--- src/components/streams-list.vue
+++ src/components/streams-list.vue
@@ -1,13 +1,22 @@
 <template>
 <div>
   <label v-text="selectCategory"></label>
-  <input v-model="inputtext" type="text" class="form-control">
+  <input v-model="inputCategoryText" type="text" class="form-control">
   <button v-on:click="SearchCategories()">検索</button>
   <div class="row">
     <div class="columns large-3 medium-6" v-for="stream in streamLists" :key="stream">
       <div class="card"> 
           <div class="stream-text">{{ stream.name }}</div>
            <img :src=stream.box_art_url v-on:click="SelectCategories(stream.id)">
+      </div>
+    </div>
+  </div>
+  <input v-model="inputChannelText" type="text" class="form-control">
+  <button v-on:click="SearchChannels()">検索</button>
+    <div class="row">
+    <div class="columns large-3 medium-6" v-for="channel in channelLists" :key="channel">
+      <div class="card"> 
+          <div class="channel-text">{{ channel.title }}</div>
       </div>
     </div>
   </div>
@@ -18,22 +27,37 @@
 export default {
   data(){
     return{
-      inputtext:"",
+      inputCategoryText:"",
+      inputChannelText:"",
       selectCategory:"",
       streamLists: [],
+      channelLists: [],
     }
   },
   methods: {
+    // ゲームカテゴリ検索
     SearchCategories() {
-      var searchkey = this.inputtext;
+      var searchkey = this.inputCategoryText;
       this.axios.get('https://api.twitch.tv/helix/search/categories?query='+searchkey)
       .then((response) => {
         console.log(response.data.data)
         this.streamLists = response.data.data;
       }).catch((error) => { console.log(error); });
     },
+
+    // 選択してるゲームカテゴリ保持
     SelectCategories(categoryId) {
       this.selectCategory = categoryId;
+    },
+
+    // 配信の検索
+    SearchChannels() {
+      var searchkey = this.inputChannelText;
+      this.axios.get('https://api.twitch.tv/helix/search/channels?query='+searchkey)
+      .then((response) => {
+        console.log(response.data.data)
+        this.channelLists = response.data.data;
+      }).catch((error) => { console.log(error); });
     },
   },
 }
@@ -88,6 +112,8 @@ h1, h2, h3, h4 {
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
+  height: 35vh;
+  overflow-y: auto;
 }
 
 .card {
